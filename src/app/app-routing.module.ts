e70c33cea2fbe53c1ef804d2ea33cd72684e5287
randikabanura <randika.banura@gmail.com>
2019-08-30 00:50:04 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardPageModule)},
+  { path: 'introduction', loadChildren: './introduction/introduction.module#IntroductionPageModule' }
 ];
 
 @NgModule({
