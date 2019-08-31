402bb2c630e093cf1f66f623f330386c3ac6824f
randikabanura <randika.banura@gmail.com>
2019-08-31 12:34:04 +0530

    Theme added

--- src/app/dashboard/settings/settings.page.ts
+++ src/app/dashboard/settings/settings.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ThemeService } from "./../../services/theme.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+  selector: "app-settings",
+  templateUrl: "./settings.page.html",
+  styleUrls: ["./settings.page.scss"]
 })
 export class SettingsPage implements OnInit {
+  status = false;
+  constructor(public theme: ThemeService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  toggleTheme() {
+    if (this.status) this.theme.enableDark();
+    else this.theme.enableLight();
   }
-
 }
